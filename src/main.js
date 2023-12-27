7749a9b0d9d2b7b5655e24fa734c956836319f73
shaokaibo <shaokaibo@qluanyun.com>
2023-12-27 18:02:30 +0800

    登录界面初始化

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'font-awesome/css/font-awesome.min.css'
+import Cookies from 'js-cookie'
+import store from './store'
+import './assets/styles/element-variables.scss'
+
+import '@/assets/styles/index.scss' // global css
+import '@/assets/styles/ruoyi.scss' // ruoyi css
+import Element from 'element-ui'
+Vue.config.productionTip = false
+
+Vue.use(Element, {
+    size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')

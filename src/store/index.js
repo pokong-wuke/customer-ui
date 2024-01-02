121d435f74b4597266491c2d2aacded014c2797b
shaokaibo <shaokaibo@qluanyun.com>
2024-01-02 17:34:57 +0800

    登录后的逻辑处理，页面布局

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
 import getters from './getters'
+import settings from './modules/settings'
+import app from './modules/app'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
+    app,
     user,
+    settings
   },
   getters
 })

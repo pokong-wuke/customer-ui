a31fbfea1abdfb0663d9cfed4c298a41a5f3ece1
贝贝 <1059366854@qq.com>
2024-01-06 22:08:50 +0800

    登录模块

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'font-awesome/css/font-awesome.min.css'
 import Cookies from 'js-cookie'
 import store from './store'
 import './assets/styles/element-variables.scss'
